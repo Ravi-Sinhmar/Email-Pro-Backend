c134905fabb0c0f1e3bf8a4df7467eb91b62854c
Ravi-Sinhmar <engineerfuture82@gmail.com>
2024-11-16 01:31:40 +0530

    updated OAuth Routes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const authorize = require("./utils/googleApiAuth");
 const { sendEmail, getUserInfo } = require("./utils/gmailServices");
 
 const corsOptions = {
-  origin: "https://email-pro-mu.vercel.app",
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
@@ -41,18 +41,37 @@ let auth;
 app.post('/authenticate', async (req, res) => {
   try {
     let auth_token = req.cookies.auth_token;
-    console.log("cookies",auth_token);
     if (!auth_token) {
       auth_token = "noToken";
     }
     auth = await authorize(auth_token);
     const token = auth.credentials.refresh_token; // Store as plain string
-   
-    res.cookie('auth_token', token, { httpOnly: true, secure: true });
+    console.log(token);
+    res.cookie('auth_token', token, { httpOnly: true, secure: false });
     res.status(200).json({ success: true, message: "Authentication successful" });
   } catch (error) {
     console.error("Authentication failed:", error);
     res.status(500).json({ success: false, message: "Authentication failed" });
+  }
+});
+
+app.get('/oauth2callback', async (req, res) => {
+  // Handle the OAuth2 callback here
+  // Parse the code from the query parameters and exchange it for tokens
+  const code = req.query.code;
+  if (code) {
+    // Exchange the code for tokens
+    try {
+      const { tokens } = await auth.getToken(code);
+      auth.setCredentials(tokens);
+      res.cookie('auth_token', tokens.refresh_token, { httpOnly: true, secure: false });
+      res.status(200).send('Authentication successful! Please return to the console.');
+    } catch (error) {
+      console.error("Error exchanging code for tokens:", error);
+      res.status(500).send('Authentication failed.');
+    }
+  } else {
+    res.status(400).send('No authentication code provided.');
   }
 });
 
